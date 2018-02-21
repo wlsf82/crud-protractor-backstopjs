6284ce6dd6e1cb3ec967931f5014a1f5f462a075
wlsf82 <wlsf82@gmail.com>
2018-02-21 23:04:22 +0100

    remove redundant method from component

--- components/computersForm/main.js
+++ components/computersForm/main.js
@@ -1,4 +1,3 @@
-const faker = require("faker");
 const helper = require("protractor-helper");
 
 const ButtonsComponent = require("./buttons");
@@ -10,19 +9,6 @@ class ComputersForm {
 
         this.buttons = new ButtonsComponent(this.container);
         this.fields = new FieldsComponent(this.container);
-    }
-
-    fillWithValidDataAndSubmit() {
-        const randomComputerName = faker.hacker.adjective();
-        const introducedDate = "2018-02-01";
-        const discontinuedDate = "2016-02-01";
-        const successMessage = `Done! Computer ${randomComputerName} has been created`;
-
-        helper.fillFieldWithTextWhenVisible(this.fields.computerName, randomComputerName);
-        helper.fillFieldWithTextWhenVisible(this.fields.introducedDate, introducedDate);
-        helper.fillFieldWithTextWhenVisible(this.fields.discontinuedDate, discontinuedDate);
-        helper.clickWhenClickable(this.buttons.createThisComputer);
-        return successMessage;
     }
 
     fillWithProvidedDataAndSubmit(data) {
