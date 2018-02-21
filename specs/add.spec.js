fafdfbddb15694493a74e12a71ebaac0566ff9cc
wlsf82 <wlsf82@gmail.com>
2018-02-21 23:03:06 +0100

    refactor test to use other component's method
    The one used before was deleted in favour of a refactoring to remo-
    ve duplicated code.

--- specs/add.spec.js
+++ specs/add.spec.js
@@ -1,3 +1,4 @@
+const faker = require("faker");
 const helper = require("protractor-helper");
 
 const CreateComputersPage = require("../page-objects/createComputers");
@@ -20,7 +21,14 @@ describe("when accessing the relative URL 'computers/new'", () => {
     describe("happy path", () => {
         describe("when providing valid data and clicking submit", () => {
             it("displays a warning message confirming the computer's successfully creation", () => {
-                const successMessage = createComputersPage.form.fillWithValidDataAndSubmit();
+                const validDataForComputerCreation = {
+                    computerName: faker.hacker.adjective(),
+                    introducedDate: "2018-02-01",
+                    discontinuedDate: "2016-02-01"
+                };
+                const successMessage = `Done! Computer ${validDataForComputerCreation.computerName} has been created`
+
+                createComputersPage.form.fillWithProvidedDataAndSubmit(validDataForComputerCreation);
 
                 helper.waitForTextToBePresentInElement(commonElements.warningMessage, successMessage);
             });
