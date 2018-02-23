863c52729d22e948b4727fc1c4394d666ba6bafd
wlsf82 <wlsf82@gmail.com>
2018-02-23 12:24:22 +0100

    remove not used code

--- visual/backstop_data/engine_scripts/chromy/clickAndHoverHelper.js
+++ visual/backstop_data/engine_scripts/chromy/clickAndHoverHelper.js
@@ -1,14 +1,6 @@
 module.exports = function (chromy, scenario) {
-    const hoverSelector = scenario.hoverSelector;
     const clickSelector = scenario.clickSelector;
     const postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
-
-    if (hoverSelector) {
-        chromy
-            .wait(hoverSelector)
-            .rect(hoverSelector)
-            .result(rect => chromy.mouseMoved(rect.left, rect.top));
-    }
 
     if (clickSelector) {
         chromy
